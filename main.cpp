7cf6185ade321a667c08e5c6422f5b2dc010b838
Ivan Viro <navioriv@gmail.com>
2018-03-04 12:55:52 -0500

    Add population and faction strucs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
 #include <iostream>
+#include <map>
+#include <numeric>
+
+enum class faction_id {
+	bandits, blacksmiths, builders, craftsmen, guards, traders, prostitutes
+};
+
+constexpr std::array<faction_id, 7> faction_ids{{faction_id::bandits, faction_id::blacksmiths, 
+	faction_id::builders, faction_id::craftsmen, faction_id::guards, faction_id::traders, faction_id::prostitutes}};
+
+
+using opinion_map = std::map<faction_id, int>;
+
+struct faction {
+	faction_id id;
+	int wealth; 	//TODO
+	int pop;
+	opinion_map opinions;
+};
+
+struct population {
+	std::map<faction_id, faction> factions;
+
+	int total() const {
+		return std::accumulate(
+			begin(factions), end(factions),
+			0,
+			[](int acc, auto const& item) {
+				faction const& fac = item.second;
+				return acc + fac.pop;
+			});
+	}
+};
 
 struct trade_good {
 	//string name;
@@ -13,8 +49,11 @@ std::ostream& operator<<(std::ostream& out, trade_good const& value) {
 
 struct region {
 	//pop = population
-	int provincial_production_value, goods_produced_mod,
-		pop;
+	int provincial_production_value;
+
+	double goods_produced_mod;
+
+	population pop;
 
 	trade_good food;
 };
@@ -23,29 +62,45 @@ std::ostream& operator<<(std::ostream& out, region const& value) {
 	return out
 		<<   "Provincial production value: " << value.provincial_production_value
 		<< "\nGoods produced mod:          " << value.goods_produced_mod
-		<< "\nPopulation:                  " << value.pop
+		<< "\nPopulation:                  " << value.pop.total()
 		<< "\nFood:                        " << value.food;
 }
 
-int region_trade_prod(int provincial_production_value, int goods_produced_mod, int pop) {
-	auto goods_produced = provincial_production_value * 2 * goods_produced_mod;
+int region_trade_prod(int provincial_production_value, double goods_produced_mod, int pop) {
+	auto goods_produced = provincial_production_value * goods_produced_mod;
 	auto local_demand = pop;
-	auto net_supply = goods_produced - local_demand;
+	auto net_supply = static_cast<int>(std::round(goods_produced - local_demand));
+	//std::cout<<net_supply;
 	return net_supply;
 }
 
-void simulate_turn_region(region r) {
-	r.food.amount = region_trade_prod(r.provincial_production_value, r.goods_produced_mod, r.pop);
+region simulate_turn(region r) {
+	r.food.amount = region_trade_prod(r.provincial_production_value, r.goods_produced_mod, r.pop.total());
 	if(r.food.amount < 0){
-		std::cout << "The people starve!\n";
-		r.pop -= r.food.amount;
+		std::cout << "The people starve!\n" << -r.food.amount << " people have died.\n";
+		//r.pop.total = std::max(0, r.pop.total() + r.food.amount);
+		int starved = static_cast<int>(-r.food.amount / static_cast<double>(faction_ids.size()));
+		for (auto& [id, fac] : r.pop.factions) {
+			fac.pop -= starved;
+		}
 	}
+	return r;
 }
 
 int main() {
 //	std::cout << "Hello, world.\n";
-	region reg1{};
-	reg1.provincial_production_value = 3;
+	region reg1 {
+		80,		// provincial production value
+//		100,		// population
+		1.00,   	// "goods produced" modifier
+		{},		//total population
+		{ 10, 1 }	// food: amount, cost
+	};
+//	std::cout << reg1 << "\n";
+	
+	reg1 = simulate_turn(reg1);
+
+
 	std::cout << reg1 << "\n";
 
 
